a5ff8dfde2dc9069bc68eb9ae670dc9c7dc504ae
Ubuntu <ubuntu@project-1.novalocal>
2015-02-03 18:01:07 +0000

    building successfully with read function pointer

--- project-1/tic-tac-toe.c
+++ project-1/tic-tac-toe.c
@@ -1,41 +1,36 @@
-#include "linux/module.h"
-#include "linux/proc_fs.h"
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
 
 static const char *proc_name = "tic-tac-toe";
 static struct proc_dir_entry *proc_entry;
-static struct *proc_fops;
 
-int read_tic_tac_toe(struct *file f, const char __user *user, size_t buffer_size, loff_t *offset) {
+ssize_t read_tic_tac_toe(struct file *f, char *buffer, size_t count, loff_t *offset) {
 	int ret = 0;
+	printk(KERN_INFO "tic-tac-toe proc file read");
 	return ret;
 }
 
+struct file_operations proc_fops = { read: read_tic_tac_toe };
 
 int init_tic_tac_toe(void) {
 	int ret = 0;
 
-	proc_entry = proc_create(proc_name, 0, NULL, proc_fops);
+	proc_entry = proc_create(proc_name, 0, NULL, &proc_fops);
 
 	if (proc_entry == NULL) {
-		printk(KERN_ERR "Tic-Tac-Toe module not loaded, not enough available memory.\n");
+		printk(KERN_ERR "tic-tac-toe module not loaded, not enough available memory.\n");
 		ret = -ENOMEM;
 	} else {
-		proc_fops = malloc(sizeof(proc_dir_entry));
-
-		if (proc_fops = NULL) {
-			printk(KERN_ERR "Tic-Tac-Toe module not loaded, not enough available memory.\n");
-			ret = -ENOMEM;
-		} else {
-			proc_fops -> read = read_tic_tac_toe
-			printk(KERN_INFO "Tic-Tac-Toe module loaded.\n");
-		}
+		printk(KERN_INFO "tic-tac-toe module loaded.\n");
 	}
 
 	return ret;
 }
 
 void deinit_tic_tac_toe(void) {
-	printk(KERN_INFO "Tic-Tac-Toe module unloaded.\n");
+	remove_proc_entry(proc_name, NULL);
+	printk(KERN_INFO "tic-tac-toe module unloaded.\n");
 }
 
 module_init(init_tic_tac_toe);
