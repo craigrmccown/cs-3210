08e5b1fcf9130b9275c1b3bb34411fc5b52060ea
Scott Vermeyen <svermeyen1@ipsec-143-215-25-91.vpn.gatech.edu>
2015-02-24 19:44:50 -0500

    interval counting + fixes to cycle counting

--- project2/clock.h
+++ project2/clock.h
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/times.h>
+#include <time.h>
 
 void access_counter(unsigned *hi, unsigned *lo);
 void start_counter();
 double get_counter();
 double mhz(int verbose, int sleeptime);
+clock_t times(struct tms *buf);
+clock_t clock(void);
+
 
