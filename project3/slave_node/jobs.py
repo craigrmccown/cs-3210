96d5f23d2c984b45d187a8849b04158eb653a14a
Craig McCown <craigrmccown@gmail.com>
2015-04-14 14:47:47 -0400

    delete from replica node job

--- project3/slave_node/jobs.py
+++ project3/slave_node/jobs.py
@@ -7,31 +7,32 @@ def replicate_file(node_id, file_hash_ring_id):
     mongo = MongoClient('localhost', 27017)
     db = mongo['rpfs_slave_db_' + str(node_id)]
     fs = GridFS(db)
-
     topology = db.topology.find()
-    sorted_v_node_ids, v_node_map = build_topology_maps(topology)
-    original_replica_node_id = get_next_v_node_id(file_hash_ring_id, sorted_v_node_ids)
-    replica_node_id = original_replica_node_id
 
-    while v_node_map[replica_node_id]['node_id'] == node_id:
-        replica_node_id = get_next_v_node_id(replica_node_id, sorted_v_node_ids)
+    replica_node = get_replica_node(topology, node_id, file_hash_ring_id)
 
-        if replica_node_id == original_replica_node_id:
-            return
+    if not replica_node:
+        return
 
-    replica_node = v_node_map.get(replica_node_id)
     f = fs.find_one({'filename': str(file_hash_ring_id)})
 
     if f:
-        response = requests.post(build_url(replica_node, file_hash_ring_id), files={'file': (str(file_hash_ring_id), f.read(), f.content_type)})
+        response = requests.post(build_url(replica_node) + '/replicate/' + str(file_hash_ring_id), files={'file': (str(file_hash_ring_id), f.read(), f.content_type)})
         assert response.status_code == 200
 
 
 def delete_from_replica_node(node_id, file_hash_ring_id):
-    # find node who is current replica node of file
-    # delete file from node
+    mongo = MongoClient('localhost', 27017)
+    db = mongo['rpfs_slave_db_' + str(node_id)]
+    topology = db.topology.find()
 
-    pass
+    replica_node = get_replica_node(topology, node_id, file_hash_ring_id)
+
+    if not replica_node:
+        return
+
+    response = requests.delete(build_url(replica_node) + '/files/' + str(file_hash_ring_id))
+    assert response.status_code == 200
 
 
 def replicate_to_new_node(node_id, new_node_id):
@@ -41,6 +42,20 @@ def replicate_to_new_node(node_id, new_node_id):
     # copy each file over
 
     pass
+
+
+def get_replica_node(topology, node_id, file_hash_ring_id):
+    sorted_v_node_ids, v_node_map = build_topology_maps(topology)
+    original_replica_node_id = get_next_v_node_id(file_hash_ring_id, sorted_v_node_ids)
+    replica_node_id = original_replica_node_id
+
+    while v_node_map[replica_node_id]['node_id'] == node_id:
+        replica_node_id = get_next_v_node_id(replica_node_id, sorted_v_node_ids)
+
+        if replica_node_id == original_replica_node_id:
+            return None
+
+    return v_node_map.get(replica_node_id)
 
 
 def build_topology_maps(topology):
@@ -63,5 +78,5 @@ def get_next_v_node_id(hash_ring_id, sorted_v_node_ids):
     return sorted_v_node_ids[0]
 
 
-def build_url(node, hash_ring_id):
-    return 'http://' + ':'.join([str(node.get('ip_address')), str(node.get('port_number'))]) + '/replicate/' + str(hash_ring_id)
+def build_url(node):
+    return 'http://' + ':'.join([str(node.get('ip_address')), str(node.get('port_number'))])
