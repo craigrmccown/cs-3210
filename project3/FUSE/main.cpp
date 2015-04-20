2561e9c4c1dac0fc841331eceaf986c641e05f67
Scott Vermeyen <svermeyen1@ipsec-143-215-27-82.vpn.gatech.edu>
2015-04-19 22:55:42 -0400

    Removing temporary read files now

--- project3/FUSE/main.cpp
+++ project3/FUSE/main.cpp
@@ -7,44 +7,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#if defined(__APPLE__)
-#  define COMMON_DIGEST_FOR_OPENSSL
-
-#  include <CommonCrypto/CommonDigest.h>
-
-#  define SHA1 CC_SHA1
-#else
-#  include <openssl/md5.h>
-#endif
-
-//MD5 Algo
-char *str2md5(const char *str, int length)
-{
-    int n;
-    MD5_CTX c;
-    unsigned char digest[16];
-    char *out = (char *) malloc(33);
-    MD5_Init(&c);
-    while (length > 0)
-    {
-        if (length > 512)
-        {
-            MD5_Update(&c, str, 512);
-        } else
-        {
-            MD5_Update(&c, str, length);
-        }
-        length -= 512;
-        str += 512;
-    }
-    MD5_Final(digest, &c);
-    for (n = 0; n < 16; ++n)
-    {
-        snprintf(&(out[n * 2]), 16 * 2, "%02x", (unsigned int) digest[n]);
-    }
-    return out;
-}
-
 //Start FUSE code
 static int pfs_getattr(const char *path, struct stat *stbuf)
 {
@@ -193,12 +155,19 @@ static int pfs_open(const char *path, struct fuse_file_info *fi)
 
 static int pfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
+    char *delete_read_path = "/tmp/rpfs/read/";
+    char delete_read[100];
     int res;
+
+    strcpy(delete_read, delete_read_path);
+    strcat(delete_read, path); // building absolute path to remove the temporary read file
+
     (void) path;
     res = pread(fi->fh, buf, size, offset);
     if (res == -1)
         res = -ENOENT;
 
+    remove(delete_read); // remove temporary read file
     return res;
 }
 
